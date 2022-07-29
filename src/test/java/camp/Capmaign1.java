5cf359b26b3fda5b75d19e1c3930ae44018e3f2d
seeth <seeth@LAPTOP-NVUVQA1O>
2022-07-29 17:30:28 +0530

    Hi Baghyashri!, hope you are doing good...Get well soon

--- src/test/java/camp/Capmaign1.java
+++ src/test/java/camp/Capmaign1.java
@@ -5,11 +5,10 @@ public class Capmaign1 {
 	public static void main(String[] args) {
 		System.out.println("Campaign1 commit");
 		System.out.println("raman");
-
-       System.out.println("bhagyashri");
-
+		System.out.println("bhagyashri");
 		System.out.println("raman1");
-
+		System.out.println("😀 😃 😄 😁 😆 😅 😂 🤣 🥲 ☺️ 😊 😇 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 😝 😜 🤪 🤨 🧐 🤓 😎 🥸 🤩 🥳 😏 😒 😞 😔 😟 😕 🙁 ☹️");
+		System.out.println("💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️💀 ☠️");
 
 	}
 
