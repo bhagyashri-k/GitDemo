eed5642489f72411dfedc21a8d1cd5869695ed30
HELLO <HELLO@DESKTOP-LF0B41U>
2022-07-29 13:29:59 +0530

    commit git

---
+++ src/test/java/github/Git1.java
@@ -0,0 +1,9 @@
+package github;
+
+public class Git1 {
+
+	public static void main(String[] args) {
+		System.out.println("Git1  pushing");
+	}
+
+}
---
+++ src/test/java/github/Git2.java
@@ -0,0 +1,9 @@
+package github;
+
+public class Git2 {
+
+	public static void main(String[] args) {
+		System.out.println("Git2  pushing");
+	}
+
+}
